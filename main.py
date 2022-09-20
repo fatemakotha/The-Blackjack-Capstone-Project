466b518bdc58e82b56ef73b759cf8c520259238b
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-20 13:14:16 +0000

    Added a function to pick a card from the deck randomly

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import random #imports the random function
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] #this is the deck a
 
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] #this is the deck available, 11 is an ACE
+
+def deal_card(): #this function chooses one card randomly
+    chosen_card = random.choice(cards)
+    print(chosen_card)
+deal_card()
