244b09333278bdc0cd39742cc4041a735dccb47c
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-20 13:44:35 +0000

    Changed from return to print

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import random
 
+def deal_card(): #function to pick a random card
+  """Returns a random card from the deck."""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  print(card)
+  
+deal_card()#prints the function
 
