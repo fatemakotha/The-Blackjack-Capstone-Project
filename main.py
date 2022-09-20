fecb339ddbf257208814cb3d2a7b9c8db49c4f78
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-20 13:16:10 +0000

    Added comments

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] #this is the deck available
 def deal_card(): #this function chooses one card randomly
     chosen_card = random.choice(cards)
     print(chosen_card)
-deal_card()
+deal_card() #calls the funtion
