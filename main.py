a2b5a34d4dafc97e01d975e825792fa2dbda0aa0
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-20 13:31:34 +0000

    Cleared all

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-import random #imports the random function
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] #this is the deck available, 11 is an ACE
-
-def deal_card(): #this function chooses one card randomly
-    chosen_card = random.choice(cards)
-    return chosen_card
-deal_card() #calls the funtion
-
-# user_cards = []
-# for cards in range (0, 1):
-#     user_cards = chosen_card.append()
-#     print(usercards)
 
