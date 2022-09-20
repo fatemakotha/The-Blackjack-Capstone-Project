706afd06f84a6342cda2787ea2dab1f4365a0ce8
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-20 13:52:28 +0000

    For loop used to pick 2 cards for the user

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ deal_card() #prints the function
 
 user_cards = [] #user cards set to an empty list
 computer_cards = [] #computer cards set to an empty list
+
+for _ in range(0,1): #for loop used to pick 2 cards for the user
+    user_cards.append(deal_card())
+print(user_cards)
