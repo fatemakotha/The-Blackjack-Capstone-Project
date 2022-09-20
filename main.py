380db477c8f9976aacc0ce7f4bc4fced0dec0f8b
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-20 13:11:41 +0000

    Added comments

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-import random
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random #imports the random function
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] #this is the deck a
 
